dbd5ced87085faa7990e69ed76752db519a3cfbc
Shengzhe Chen <shengzhe.chen@gmail.com>
2021-01-08 23:47:25 -0800

    [selenium]page_response

--- chrome_sample_test.py
+++ chrome_sample_test.py
@@ -3,6 +3,10 @@ from selenium import webdriver
 
 driver = webdriver.Chrome()
 driver.get('http://www.google.com/')
+breakpoint()
+print(driver.page_response)
+print(driver)
+print(dir(driver))
 time.sleep(5)
 search_box = driver.find_element_by_name('q')
 search_box.send_keys('ChromDriver')
